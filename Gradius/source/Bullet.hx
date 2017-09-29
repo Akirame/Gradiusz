2d3ddde17f2f138b409be685d0189601cb9da2fd
G <gsvillalba12@hotmail.com>
2017-09-28 22:16:42 -0300

    Change

---
+++ Gradius/source/Bullet.hx
@@ -0,0 +1,29 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import source.Reg;
+
+/**
+ * ...
+ * @author G
+ */
+class Bullet extends FlxSprite 
+{
+
+	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
+	{
+		super(X, Y, SimpleGraphic);
+		
+	}
+	
+	override public function update(elapsed:Float):Void 
+	{
+		super.update(elapsed);
+		velocity.set(Reg.camVelocityX + 250, 0);
+		if (x > 1000)
+			kill();
+		
+	}
+	
+}
--- Gradius/source/Player.hx
+++ Gradius/source/Player.hx
@@ -2,6 +2,7 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.system.FlxAssets.FlxGraphicAsset;
 import source.Reg;
 
@@ -11,10 +12,13 @@ import source.Reg;
  */
 class Player extends FlxSprite 
 {
-
+	private var playerBullet:Bullet;
+	private var bulletGroup:FlxTypedGroup<Bullet>;
+	
 	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
 	{
 		super(X, Y, SimpleGraphic);
+		bulletGroup = new FlxTypedGroup();
 	}
 	
 	override public function update(elapsed:Float):Void 
@@ -23,7 +27,12 @@ class Player extends FlxSprite
 		velocity.set(Reg.camVelocityX, 0);
 		
 		PlayerMovement();
-						
+		if (FlxG.keys.justPressed.K)
+		{
+			playerBullet = new Bullet(x,y, AssetPaths.playerBullet__png);
+			bulletGroup.add(playerBullet);
+			FlxG.state.add(bulletGroup);
+		}
 	}
 	
 	function PlayerMovement():Void 
